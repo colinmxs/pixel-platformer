ecea66f2657b87bf8b5c16b5391605b44371aa45
Colin Smith <smith.colin00@gmail.com>
2020-12-31 16:37:29 -0700

    add cameracontroller

---
+++ Pixel Platformer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿namespace PixelPlatformer
+{
+    using UnityEngine;
+
+    public class CameraController : MonoBehaviour
+    {
+        public float constYPos;
+        public float interpVelocity;
+        public float minDistance;
+        public float followDistance;
+        public GameObject target;
+        public Vector3 offset;
+        Vector3 targetPos;
+        
+        private void Start()
+        {
+            targetPos = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            if (target)
+            {
+                Vector3 posNoZ = transform.position;
+                posNoZ.z = target.transform.position.z;
+
+                Vector3 targetDirection = (target.transform.position - posNoZ);
+
+                interpVelocity = targetDirection.magnitude * 5f;
+
+                targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+                targetPos.y = constYPos;
+                transform.position = Vector3.Lerp(transform.position, targetPos + offset, 1f);
+            }
+        }
+    }
+}
