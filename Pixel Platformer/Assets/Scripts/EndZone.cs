78817902803d60def404ea0cb1ef57710c298545
Colin Smith <smith.colin00@gmail.com>
2021-01-04 00:32:37 -0700

    add endzone script

---
+++ Pixel Platformer/Assets/Scripts/EndZone.cs
@@ -0,0 +1,21 @@
+﻿namespace PixelPlatformer
+{
+    using UnityEngine;
+
+    public class EndZone : MonoBehaviour
+    {
+        [SerializeField]
+        private Stopwatch _stopWatch;
+        [SerializeField]
+        private Screamer _screamer;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            var particles = GetComponent<ParticleSystem>();
+            particles.Play();
+            GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().Stop();
+            _stopWatch.StopTimer();
+            _screamer.PlayEndSong();
+        }
+    }
+}
