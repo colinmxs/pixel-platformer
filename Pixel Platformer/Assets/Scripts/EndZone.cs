27751bf9b42aa812f16a420d520d60581d266f2b
Colin Smith <smith.colin00@gmail.com>
2021-01-04 22:11:42 -0700

    blink time on end

--- Pixel Platformer/Assets/Scripts/EndZone.cs
+++ Pixel Platformer/Assets/Scripts/EndZone.cs
@@ -1,21 +1,33 @@
 ﻿namespace PixelPlatformer
 {
     using UnityEngine;
+    using UnityEngine.UI;
 
     public class EndZone : MonoBehaviour
     {
         [SerializeField]
         private Stopwatch _stopWatch;
         [SerializeField]
+        private Text _finalMessageText;
+        [SerializeField]
         private Screamer _screamer;
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            var particles = GetComponent<ParticleSystem>();
-            particles.Play();
+            if(_stopWatch.CurrentTime < 60000)
+            {
+                _finalMessageText.text = "SNEAKY FAST!!! CONGRATULATIONS";
+                var particles = GetComponent<ParticleSystem>();
+                particles.Play();
+                _screamer.PlayEndSong();
+            }
+            else
+            {
+                _finalMessageText.text = "Try to finish in under a minute for an alternate ending. press 'r' to restart.";
+            }
+            
             GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().Stop();
             _stopWatch.StopTimer();
-            _screamer.PlayEndSong();
         }
     }
 }
