18fb842268b6abfc7716d006e851a328ef54da4d
Colin Smith <smith.colin00@gmail.com>
2021-01-04 22:12:27 -0700

    add blink method

--- Pixel Platformer/Assets/Scripts/Stopwatch.cs
+++ Pixel Platformer/Assets/Scripts/Stopwatch.cs
@@ -9,6 +9,7 @@
     public class Stopwatch : MonoBehaviour
     {
         public double CurrentTime { get; set; }
+        public string FormattedCurrentTime { get; private set; }
 
         private Text text;
         private bool breaker = false;
@@ -25,15 +26,39 @@
         internal void StopTimer()
         {
             breaker = true;
+            StartCoroutine(BlinkCharacter(this.gameObject));
         }
+        public IEnumerator BlinkCharacter(GameObject player)
+        {
+            var sprites = player.GetComponent<Text>();
+            bool blink = false;
+            float blinkTime = 4f;
+            while (blinkTime > 0)
+            {
+                blink = !blink;
+                if (blink)
+                {
+                    sprites.enabled = false;
+                }
+                else
+                {
+                    sprites.enabled = true;
+                }
+                blinkTime -= .1f;
+                yield return new WaitForSeconds(.1f);
+            }
 
+            
+            sprites.enabled = true;
+            
+        }
         IEnumerator Run()
         {
             int millis = 0;
 
             while (!breaker)
             {
-                millis += 100;
+                millis += 10;
 
                 double totalMilliseconds = millis;
                 var totalSeconds = totalMilliseconds / 1000;
@@ -41,8 +66,9 @@
                 var minutes = Math.Floor(totalSeconds / 60);
                 var seconds = totalSeconds % 60;
                 CurrentTime = totalMilliseconds;
-                text.text = $"{minutes}:{seconds.ToString("00.00")}";
-                yield return new WaitForSeconds(.1f);
+                FormattedCurrentTime = $"{minutes}:{seconds.ToString("00.00")}";
+                text.text = FormattedCurrentTime;
+                yield return new WaitForSeconds(.01f);
             }
         }
     }
